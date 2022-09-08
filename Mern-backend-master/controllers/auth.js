f7c45ff3d87bf5dd42a9ab20f21778e6bf59f1e5
lohith2 <54168802+lohith2@users.noreply.github.com>
2022-09-07 18:19:24 -0700

    first commit

---
+++ Mern-backend-master/controllers/auth.js
@@ -0,0 +1,124 @@
+const User = require("../models/user");
+const { check, validationResult } = require("express-validator");
+var jwt = require("jsonwebtoken");
+var expressJwt = require("express-jwt");
+var nodemailer = require('nodemailer');
+var smtpTransport = require('nodemailer-smtp-transport');
+var handlebars = require('handlebars');
+var fs = require('fs');
+var template;
+var replacements;
+exports.signup = (req, res) => {
+  const errors = validationResult(req);
+
+  if (!errors.isEmpty()) {
+    return res.status(422).json({
+      error: errors.array()[0].msg
+    });
+  }
+  const user = new User(req.body);
+  user.save((err, user) => {
+    if (err) {
+      return res.status(400).json({
+        err: "NOT able to save user in DB"
+      });
+    }
+    res.json({
+      name: user.name,
+      email: user.email,
+      id: user._id
+    });
+    
+var transporter = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+      user: 'lohitreddygms123@gmail.com',
+      pass: ''
+    }
+  });
+  
+  
+  var mailOptions = {
+    from: 'lohitreddygms123@gmail.com',
+    to: user.email,
+    subject: 'Welcome to freakstore',
+    html: '<p>Hey there,</p><br></br><p>Explore our new collections.I am sure that you will love them</p><br></br><b>Find it, love it, buy it </b>'
+  };
+  
+  transporter.sendMail(mailOptions, function(error, info){
+    if (error) {
+      console.log(error);
+    } else {
+      console.log('Email sent: ' + info.response);
+    }
+  });
+
+  });
+};
+
+exports.signin = (req, res) => {
+  const errors = validationResult(req);
+  const { email, password } = req.body;
+
+  if (!errors.isEmpty()) {
+    return res.status(422).json({
+      error: errors.array()[0].msg
+    });
+  }
+
+  User.findOne({ email }, (err, user) => {
+    if (err || !user) {
+      return res.status(400).json({
+        error: "USER email does not exists"
+      });
+    }
+
+    if (!user.autheticate(password)) {
+      return res.status(401).json({
+        error: "Email and password do not match"
+      });
+    }
+
+    //create token
+    const token = jwt.sign({ _id: user._id }, process.env.SECRET);
+    //put token in cookie
+    res.cookie("token", token, { expire: new Date() + 9999 });
+
+    //send response to front end
+    const { _id, name, email, role } = user;
+    return res.json({ token, user: { _id, name, email, role } });
+  });
+};
+
+exports.signout = (req, res) => {
+  res.clearCookie("token");
+  res.json({
+    message: "User signout successfully"
+  });
+};
+
+//protected routes
+exports.isSignedIn = expressJwt({
+  secret: process.env.SECRET,
+  userProperty: "auth"
+});
+
+//custom middlewares
+exports.isAuthenticated = (req, res, next) => {
+  let checker = req.profile && req.auth && req.profile._id == req.auth._id;
+  if (!checker) {
+    return res.status(403).json({
+      error: "ACCESS DENIED"
+    });
+  }
+  next();
+};
+
+exports.isAdmin = (req, res, next) => {
+  if (req.profile.role === 0) {
+    return res.status(403).json({
+      error: "You are not ADMIN, Access denied"
+    });
+  }
+  next();
+};
